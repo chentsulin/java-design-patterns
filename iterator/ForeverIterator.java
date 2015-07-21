2625ac78555d0e5df5e05eb26eab6f044d0d1b3e
C. T. Lin <chentsulin@gmail.com>
2015-07-21 12:23:15 +0800

    ForeverIterator.java

---
+++ iterator/ForeverIterator.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class ForeverIterator {
+
+  public boolean hasNext() {
+    return true;
+  }
+  
+  public Object next() {
+    return new Object();
+  }
+  
+  public void remove() {
+    //  
+  }
+
+}
